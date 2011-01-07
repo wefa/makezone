e310a6f0f771bd8bb4331f42a28cf17afbb4606a
rpolzer <rpolzer@0942012a-5dd3-de11-ba86-00215a508788>
2011-01-07 14:26:03 +0000

    add a new unit test for CIDR delegations
    git-svn-id: svn://127.0.0.1/nxcon/makezone/trunk@141 0942012a-5dd3-de11-ba86-00215a508788

---
+++ tests/delegator/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+../makezone-runner.sh \
+	-f lab.example.org	out/db.org.example.lab \
+	-r 192.168.0.0/16	out/db.192.168 \
+	
--- tests/makezone-runner.sh
+++ tests/makezone-runner.sh
@@ -2,7 +2,7 @@
 
 ln -snf ../../beautify.awk .
 cp "src" "src.tmp"
-../../makezone "src.tmp" "$@"
+../../makezone "src.tmp" -short "$@"
 rm -f "src.tmp"
 ret=$?
 rm -f beautify.awk
