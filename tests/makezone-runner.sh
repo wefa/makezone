6e52b7c447ed4e35029bfff0e7d887bd93066283
rpolzer <rpolzer@0942012a-5dd3-de11-ba86-00215a508788>
2012-05-04 15:03:54 +0000

    no longer beautify in the test run
    git-svn-id: svn://127.0.0.1/nxcon/makezone/trunk@2139 0942012a-5dd3-de11-ba86-00215a508788

--- tests/makezone-runner.sh
+++ tests/makezone-runner.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-ln -snf ../../beautify.awk .
 cp "src" "src.tmp"
 ../../makezone "src.tmp" -short "$@" && awk -f ../../makehosts.awk src.tmp | perl ../../sorthosts.pl > out/hosts
 ret=$?
